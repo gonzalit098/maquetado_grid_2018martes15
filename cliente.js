a97dfb808131a59ac9fbb7e3a35a6ce7ec79c784
Glitch (maquetado-gon) <none>
2018-07-10 20:16:30 +0000

    🌏💠 Updated with Glitch

---
+++ cliente.js
@@ -0,0 +1,26 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+
+
+class Distribuidora {
+  
+  static ofertar(){
+console.log('[..] Programando en Javascript en Buenos Aires');
+    
+    //document.querySelector("#panelMsg").innerHTML += "Algo!" ;
+    document.querySelector("#panelMsg").innerHTML += document.querySelector("#golosina_titulo").value
+    + document.querySelector("#golosina_descripcion").value;
+    
+    
+    
+    
+    //alert(1235678);
+    
+console.log('[ok] Programando en Javascript en Buenos Aires');  
+  }
+  
+}
+  
+  
